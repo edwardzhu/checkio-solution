1a150cb57171212358b84e351a0c073baa83d9fd
Edward Zhu <edward.zr.zhu@gmail.com>
2014-02-28 11:25:45 +0800

    Fix the issue on Xs or Os problem.

--- Home/xsOros.py
+++ Home/xsOros.py
@@ -1,9 +1,9 @@
 def checkio(array):
-    if array[0][0] == array[0][1] == array[0][2] or array[0][0] == array[1][0] == array[2][0] or array[0][0] == array[1][1] == array[2][2]:
+    if (array[0][0] == array[0][1] == array[0][2] or array[0][0] == array[1][0] == array[2][0] or array[0][0] == array[1][1] == array[2][2]) and array[0][0] != '.':
         return array[0][0]
-    if array[1][0] == array[1][1] == array[1][2] or array[0][1] == array[1][1] == array[2][1] or array[2][0] == array[1][1] == array[0][2]:
+    if (array[1][0] == array[1][1] == array[1][2] or array[0][1] == array[1][1] == array[2][1] or array[2][0] == array[1][1] == array[0][2]) and array[1][1] != '.':
         return array[1][1]
-    if array[2][0] == array[2][1] == array[2][2] or array[0][2] == array[1][2] == array[2][2]:
+    if (array[2][0] == array[2][1] == array[2][2] or array[0][2] == array[1][2] == array[2][2]) and array[2][2] != '.' :
         return array[2][2]
     return "D"
 
@@ -20,3 +20,8 @@ if __name__ == '__main__':
     "OOX",
     "XXO",
     "OXX"]) == "D", "Draw"
+    assert checkio([
+        "...",
+        "XXO",
+        "XXO"
+    ]) == "D", "Draw"
